13c7f3f9741163b8bbb2e52fe00a873c80564e01
DTeltsov <63288770+DTeltsov@users.noreply.github.com>
2021-12-16 21:47:39 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
-token = '1601690823:AAF_S8zbMtAgT1ZMgHdeSETSPjPONmL1w8s'
-api_key = 'f14f453bef90845e9e492f7f1edfdd0b'
+token = 'token'
+api_key = 'api_key'
 
 USER_STATE = defaultdict(lambda: {})
 graph, commands = range(2)
